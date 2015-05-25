7a08771b2fef5cdc92617af90d2eaa778452513f
Marcelo Fabri <marcelofabrimf@gmail.com>
2015-05-25 14:32:59 -0300

    Initial commit

---
+++ Example/OverlayView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  OverlayView
+//
+//  Created by Marcelo Fabri on 05/25/2015.
+//  Copyright (c) 05/25/2015 Marcelo Fabri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
